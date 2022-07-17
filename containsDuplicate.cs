b16975f64d8b9a5904e5055b560db01619eda194
socraticDevBlog <41236715+socraticDevBlog@users.noreply.github.com>
2022-07-17 16:51:05 -0400

    Update containsDuplicate.cs

--- containsDuplicate.cs
+++ containsDuplicate.cs
@@ -1,20 +1,12 @@
 public class Solution {
     public bool ContainsDuplicate(int[] nums) {
-        Array.Sort(nums);
-            
-        bool containsDuplicate = false;
-                    
-        for (int i = 0; i<nums.Count(); i++) {
-            if (i == nums.Count()) {
-                continue;
-            }
-            
-            if (nums[i] == nums[i+1]){
-                containsDuplicate = true;
-                break;
-            }
-        }
-        
-        return containsDuplicate;
+        var list = new List<int>();
+        list.AddRange(nums);
+        var distinctList = list.Distinct().ToList();
+        return list.Count() != distinctList.Count(); 
     }
 }
+
+// Runtime: 263 ms, faster than 57.21% of C# online submissions for Contains Duplicate.
+// Memory Usage: 47.4 MB, less than 48.39% of C# online submissions for Contains Duplicate.
+(2022-07-17)
